6fe2dd87413a901fd6cd3483e46f20b0ab541583
Freshta-Jafari <jafari.freshta2024@gmail.com>
2024-09-22 12:07:16 +0200

    new projet php

---
+++ Bienvenu.php
@@ -0,0 +1,65 @@
+<?php
+if (isset($_GET['delete'])){
+    $lineToDelete = intval($_GET['delete']);
+    $line = file('data.txt' , FILE_IGNORE_NEW_LINES);
+    unset($lines[$lineToDelete]);
+    file_put_contents('data.txt', implode('\n', $lines));
+    header("Location: Bienvenu.php");
+    exit();
+}
+
+?>
+
+<html>
+    <head>
+   
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel = "stylesheet" href = "style.css">
+        
+    </head>
+<body>
+
+   <h1>Votre Informations</h1> 
+
+<table>
+    <thead>
+    <tr>
+        <th>Prenom/Nom</th>
+        <th>Email</th>
+    </tr>
+    </thead>
+    <?php
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST"){
+        $name = htmlspecialchars($_POST['name']);
+        $email = htmlspecialchars($_POST['email']);
+
+        // Enregister informations 
+        $data = "$name, $email\n";
+        file_put_contents('data.txt' , $data, FILE_APPEND);
+    
+    }
+
+    // display enregistred informatios 
+    if (file_exists('data.txt')){
+        $lines = file('data.txt');
+        foreach ($lines as $line){
+            $row = explode(',', trim($line));
+            echo "<tr>";
+            foreach ($row as $cell){
+                echo "<td>" . htmlspecialchars($cell) . "</td>";
+            }
+            echo "</tr>";
+        }
+    }
+
+    ?>
+
+
+</table>
+
+<button onclick="window.history.back()">Reture</button>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,22 @@
+<html lang="fa">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel = "stylesheet" href = "style.css">
+        <title>Entrez les informations</title>
+    </head>
+<body>
+
+<form action = "Bienvenu.php" method = "POST">
+<label for = "name">Prenome / Nom :</label>
+<input type="text" name="name" required><br>
+<label for = "email">E-mail: </label>
+<input type = "text" name = "email"required><br>
+<input type = "submit">
+</form>
+
+
+
+
+</body>
+</html>
