059abd031a91ff00d7387435f6a019090e978507
Freshta-Jafari <jafari.freshta2024@gmail.com>
2024-09-23 13:32:28 +0200

    ajouter styles en CSS

--- index.php
+++ index.php
@@ -6,15 +6,19 @@
         <title>Entrez les informations</title>
     </head>
 <body>
+    <div class="form-container">
+<h1>Entrez les informations</h1>
 
 <form action = "Bienvenu.php" method = "POST">
+
+
 <label for = "name">Prenome / Nom :</label>
 <input type="text" name="name" required><br>
 <label for = "email">E-mail: </label>
 <input type = "text" name = "email"required><br>
 <input type = "submit">
 </form>
-
+</div>
 
 
 
